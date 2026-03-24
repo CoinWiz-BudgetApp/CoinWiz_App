614466d4a7dc68c64398a64dc8aa6153a9b6d222
Jack Brunswik <jbrunswik5@gmail.com>
2026-03-23 22:58:16 -0400

    Commented out database deletion section (which is for testing purposes)

--- database/db.ts
+++ database/db.ts
@@ -1,13 +1,26 @@
 import * as SQLite from 'expo-sqlite';
 
-export const db = SQLite.openDatabaseSync('coinwiz.db');
+export const db = SQLite.openDatabaseAsync('coinwiz.db');
 
-export function initDB() {
-  db.execSync(`
+export async function initDB() {
+  const database = await db;
+
+  await database.execAsync(`
     CREATE TABLE IF NOT EXISTS users (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       username TEXT UNIQUE,
-      password TEXT
+      password TEXT,
+      bankLinked INTEGER DEFAULT 0
     );
   `);
 }
+
+/* export async function clearDatabase() {
+  const database = await db;
+
+  await database.execAsync(`
+    DELETE FROM users;
+  `);
+
+  console.log("Database cleared");
+} */
